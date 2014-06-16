9b100b1b50ad515facfa919a92af03a3c23be572
karanshah126 <karanshah126@gmail.com>
2014-06-17 01:34:58 +0530

    removed whitespace
    improved UI, textfield sanitization, added sqrt function

--- src/AppTest1.java
+++ src/AppTest1.java
@@ -15,8 +15,7 @@ import javax.swing.*;
 public class AppTest1 extends JFrame{
 
 	JPanel mainPanel;
-	JLabel valueLabel;
-	JButton equalButton,addButton,subButton,multiplyButton,divideButton,resetButton;
+	JButton equalButton,addButton,subButton,multiplyButton,divideButton,resetButton,sqrtButton;
 	JTextField textField;
 	
 	
@@ -33,37 +32,52 @@ public class AppTest1 extends JFrame{
 	
 	public AppTest1()
 	{
-		this.setSize(600, 444);
+		this.setSize(369, 338);
 		this.setLocationRelativeTo(null);
 		
 		this.setResizable(false);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle("Calculator and Converter");
 		mainPanel=new JPanel();
-		valueLabel=new JLabel("Value:");
-		valueLabel.setBounds(232, 13, 40, 19);
-		valueLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		mainPanel.setBackground(new Color(0, 153, 204));
 		
 		
 		ListenForButton buttonListen = new ListenForButton();
 		
-		equalButton=new JButton("CALC");
-		equalButton.setBounds(120, 160, 100, 40);
-		equalButton.setFont(new Font("Tahoma", Font.BOLD, 13));
+		equalButton=new JButton("=");
+		equalButton.setForeground(new Color(51, 0, 255));
+		equalButton.setBackground(new Color(135, 206, 250));
+		equalButton.setBounds(194, 121, 100, 40);
+		equalButton.setFont(new Font("Tahoma", Font.BOLD, 19));
 		addButton=new JButton("+");
-		addButton.setBounds(80, 85, 70, 40);
+		addButton.setForeground(new Color(51, 0, 255));
+		addButton.setBackground(new Color(135, 206, 250));
+		addButton.setBounds(53, 64, 50, 40);
 		addButton.setFont(new Font("Tahoma", Font.BOLD, 19));
 		subButton=new JButton("-");
-		subButton.setBounds(180, 85, 70, 40);
+		subButton.setForeground(new Color(51, 0, 255));
+		subButton.setBackground(new Color(135, 206, 250));
+		subButton.setBounds(117, 64, 50, 40);
 		subButton.setFont(new Font("Tahoma", Font.BOLD, 19));
-		multiplyButton=new JButton("X");
-		multiplyButton.setBounds(280, 85, 70, 40);
+		multiplyButton=new JButton("x");
+		multiplyButton.setForeground(new Color(51, 0, 255));
+		multiplyButton.setBackground(new Color(135, 206, 250));
+		multiplyButton.setBounds(184, 64, 50, 40);
 		multiplyButton.setFont(new Font("Tahoma", Font.BOLD, 19));
 		divideButton=new JButton("/");
-		divideButton.setBounds(380, 85, 70, 40);
+		divideButton.setForeground(new Color(51, 0, 255));
+		divideButton.setBackground(new Color(135, 206, 250));
+		divideButton.setBounds(254, 64, 50, 40);
 		divideButton.setFont(new Font("Tahoma", Font.BOLD, 19));
+		sqrtButton=new JButton("sq. root");
+		sqrtButton.setForeground(new Color(51, 0, 255));
+		sqrtButton.setBackground(new Color(135, 206, 250));
+		sqrtButton.setBounds(53, 121, 114, 40);
+		sqrtButton.setFont(new Font("Tahoma", Font.BOLD, 19));
 		resetButton=new JButton("RESET");
-		resetButton.setBounds(300, 160, 100, 40);
+		resetButton.setForeground(new Color(51, 0, 255));
+		resetButton.setBackground(new Color(135, 206, 250));
+		resetButton.setBounds(129, 174, 100, 40);
 		resetButton.setFont(new Font("Tahoma", Font.BOLD, 13));
 		
 		equalButton.addActionListener(buttonListen);
@@ -71,24 +85,34 @@ public class AppTest1 extends JFrame{
 		subButton.addActionListener(buttonListen);
 		multiplyButton.addActionListener(buttonListen);
 		divideButton.addActionListener(buttonListen);
+		sqrtButton.addActionListener(buttonListen);
 		resetButton.addActionListener(buttonListen);
 		
 		textField=new JTextField("0",15);
-		textField.setBounds(156, 42, 231, 28);
+		textField.setForeground(new Color(51, 0, 255));
+		textField.setBounds(53, 23, 251, 28);
 		textField.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		textField.selectAll();
 		
 		kgLbButton = new JButton("Kg. --> lb.");
-		kgLbButton.setBounds(251, 295, 120, 25);
+		kgLbButton.setForeground(new Color(51, 0, 255));
+		kgLbButton.setBackground(new Color(135, 206, 250));
+		kgLbButton.setBounds(53, 227, 120, 25);
 		kgLbButton.setFont(new Font("Tahoma", Font.BOLD, 13));
 		lbKgButton = new JButton("lb. --> Kg.");
-		lbKgButton.setBounds(251, 333, 120, 25);
+		lbKgButton.setForeground(new Color(51, 0, 255));
+		lbKgButton.setBackground(new Color(135, 206, 250));
+		lbKgButton.setBounds(53, 265, 120, 25);
 		lbKgButton.setFont(new Font("Tahoma", Font.BOLD, 13));
 		kmMlButton = new JButton("Km. --> ml.");
-		kmMlButton.setBounds(381, 333, 120, 25);
+		kmMlButton.setForeground(new Color(51, 0, 255));
+		kmMlButton.setBackground(new Color(135, 206, 250));
+		kmMlButton.setBounds(184, 265, 120, 25);
 		kmMlButton.setFont(new Font("Tahoma", Font.BOLD, 13));
 		mlKmButton = new JButton("ml. --> Km.");
-		mlKmButton.setBounds(381, 295, 120, 25);
+		mlKmButton.setForeground(new Color(51, 0, 255));
+		mlKmButton.setBackground(new Color(135, 206, 250));
+		mlKmButton.setBounds(184, 227, 120, 25);
 		mlKmButton.setFont(new Font("Tahoma", Font.BOLD, 13));
 		
 		kgLbButton.addActionListener(buttonListen);
@@ -97,13 +121,12 @@ public class AppTest1 extends JFrame{
 		mlKmButton.addActionListener(buttonListen);
 		
 		mainPanel.setLayout(null);
-	
-		mainPanel.add(valueLabel);
 		mainPanel.add(textField);
 		mainPanel.add(addButton);
 		mainPanel.add(subButton);
 		mainPanel.add(multiplyButton);
 		mainPanel.add(divideButton);
+		mainPanel.add(sqrtButton);
 		mainPanel.add(equalButton);
 		mainPanel.add(resetButton);
 		mainPanel.add(kgLbButton);
@@ -121,6 +144,31 @@ public class AppTest1 extends JFrame{
 		
 		
 	}
+	
+	private boolean isNumeric()  
+	{  
+	  try  
+	  {  
+	    double d = Double.parseDouble(textField.getText());  
+	    return true; 
+	  }  
+	  catch(NumberFormatException nfe)  
+	  {  
+		 msgbox("Please enter valid number!");
+		 clearTextField();
+	    return false;  
+	  }     
+	}
+	
+	private void msgbox(String s){
+		   JOptionPane.showMessageDialog(null, s);
+		}
+	
+	private void clearTextField()
+	{
+        textField.setText("");
+        textField.grabFocus();
+	}
 
 
 
@@ -136,51 +184,60 @@ class ListenForButton implements ActionListener{
 	        // Check if the source of the event was the button
 	         
 	        
-	        if(e.getSource() == addButton){
+	        if(e.getSource() == addButton && isNumeric() == true){
 	        	
-	          
+	         
 	        	   num1=Double.parseDouble(textField.getText());
 	               
-	               textField.setText("");
-	               textField.grabFocus();
+	        	   clearTextField();
 	               decide=1;
 	        	   }
 	        
 	        
-	        if(e.getSource() == subButton){
+	        if(e.getSource() == subButton && isNumeric() == true){
 	        	
 	          
 	        	   num1=Double.parseDouble(textField.getText());
 	               
-	               textField.setText("");
-	               textField.grabFocus();
+	        	   clearTextField();
 	               decide=2;
 	        	   }
 	        	
 	        
-	        if(e.getSource() == multiplyButton){
+	        if(e.getSource() == multiplyButton && isNumeric() == true){
 	        	
 	          
 	        	   num1=Double.parseDouble(textField.getText());
 	               
-	               textField.setText("");
-	               textField.grabFocus();
+	        	   clearTextField();
 	               decide=3;
 	        	   }
 	        
 	        
-	        if(e.getSource() == divideButton){
+	        if(e.getSource() == divideButton  && isNumeric() == true){
 	        	
 	          
 	        	   num1=Double.parseDouble(textField.getText());
-	               
-	               textField.setText("");
-	               textField.grabFocus();
+	        	   
+	        	   clearTextField();
 	               decide=4;
 	        	   }
 	        
+	        if(e.getSource() == sqrtButton  && isNumeric() == true){
 	        
-	        if(e.getSource() == equalButton)
+            double NumberX = Double.parseDouble(textField.getText());
+            
+            	if(NumberX>=0)
+            	textField.setText(String.valueOf(Math.sqrt(NumberX)));
+            	
+            	else{
+            	msgbox("Number can not be negative.");
+            	clearTextField();
+            	}
+            
+	        }
+	        
+	        if(e.getSource() == equalButton  && isNumeric() == true)
 	        {
 	        	num2=Double.parseDouble(textField.getText());
 	        	if(decide == 1)
@@ -209,22 +266,22 @@ class ListenForButton implements ActionListener{
 	        if(e.getSource() == resetButton)
 	        {
 	        	num1=0;num2=0;decide=0;result=0;
-	        	textField.setText("");textField.grabFocus();
+	        	clearTextField();
 	        }
 	        
-	        if(e.getSource() == kmMlButton)
+	        if(e.getSource() == kmMlButton  && isNumeric() == true)
 	        {
 	        	textField.setText(0.621371*(Double.parseDouble(textField.getText()))+"");
 	        }
-	        if(e.getSource() == mlKmButton)
+	        if(e.getSource() == mlKmButton  && isNumeric() == true)
 	        {
 	        	textField.setText(1.609371*(Double.parseDouble(textField.getText()))+"");
 	        }
-	        if(e.getSource() == kgLbButton)
+	        if(e.getSource() == kgLbButton  && isNumeric() == true)
 	        {
 	        	textField.setText(2.20462*(Double.parseDouble(textField.getText()))+"");
 	        }
-	        if(e.getSource() == lbKgButton)
+	        if(e.getSource() == lbKgButton  && isNumeric() == true)
 	        {
 	        	textField.setText(0.453592*(Double.parseDouble(textField.getText()))+"");
 	        }
