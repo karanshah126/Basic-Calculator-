424ffe2d5ef7a93e4574f0272425e5f68cd992c5
karanshah126 <karanshah126@gmail.com>
2014-06-16 19:45:29 +0530

    refactored Variable names

--- src/AppTest1.java
+++ src/AppTest1.java
@@ -14,19 +14,19 @@ import javax.swing.*;
 
 public class AppTest1 extends JFrame{
 
-	JPanel Pan;
-	JLabel LB1;
-	JButton Button1,badd,bsub,bmul,bdiv,breset;
-	JTextField TF1;
-	int BCount;
+	JPanel mainPanel;
+	JLabel valueLabel;
+	JButton equalButton,addButton,subButton,multiplyButton,divideButton,resetButton;
+	JTextField textField;
 	
-	float result,cres;
-    float num1=0,num2=0;int decide=0;
-    JTextField TFCon;
-    JButton bkg2lb,blb2kg,bkm2ml,bml2km;
+	
+	double result;
+    double num1=0,num2=0;int decide=0;
+    JButton kgLbButton,lbKgButton,kmMlButton,mlKmButton;
 	
 	
 	public static void main(String[] args) {
+	
 		
 		new AppTest1();
 	}
@@ -39,85 +39,81 @@ public class AppTest1 extends JFrame{
 		this.setResizable(false);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle("Calculator and Converter");
-		Pan=new JPanel();
-		LB1=new JLabel("Value:");
-		LB1.setBounds(232, 13, 40, 19);
-		LB1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		mainPanel=new JPanel();
+		valueLabel=new JLabel("Value:");
+		valueLabel.setBounds(232, 13, 40, 19);
+		valueLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		
 		
-		ListenForButton bl = new ListenForButton();
+		ListenForButton buttonListen = new ListenForButton();
 		
-		Button1=new JButton("CALC");
-		Button1.setBounds(120, 160, 100, 40);
-		Button1.setFont(new Font("Tahoma", Font.BOLD, 13));
-		badd=new JButton("+");
-		badd.setBounds(80, 85, 70, 40);
-		badd.setFont(new Font("Tahoma", Font.BOLD, 19));
-		bsub=new JButton("-");
-		bsub.setBounds(180, 85, 70, 40);
-		bsub.setFont(new Font("Tahoma", Font.BOLD, 19));
-		bmul=new JButton("X");
-		bmul.setBounds(280, 85, 70, 40);
-		bmul.setFont(new Font("Tahoma", Font.BOLD, 19));
-		bdiv=new JButton("/");
-		bdiv.setBounds(380, 85, 70, 40);
-		bdiv.setFont(new Font("Tahoma", Font.BOLD, 19));
-		breset=new JButton("RESET");
-		breset.setBounds(300, 160, 100, 40);
-		breset.setFont(new Font("Tahoma", Font.BOLD, 13));
+		equalButton=new JButton("CALC");
+		equalButton.setBounds(120, 160, 100, 40);
+		equalButton.setFont(new Font("Tahoma", Font.BOLD, 13));
+		addButton=new JButton("+");
+		addButton.setBounds(80, 85, 70, 40);
+		addButton.setFont(new Font("Tahoma", Font.BOLD, 19));
+		subButton=new JButton("-");
+		subButton.setBounds(180, 85, 70, 40);
+		subButton.setFont(new Font("Tahoma", Font.BOLD, 19));
+		multiplyButton=new JButton("X");
+		multiplyButton.setBounds(280, 85, 70, 40);
+		multiplyButton.setFont(new Font("Tahoma", Font.BOLD, 19));
+		divideButton=new JButton("/");
+		divideButton.setBounds(380, 85, 70, 40);
+		divideButton.setFont(new Font("Tahoma", Font.BOLD, 19));
+		resetButton=new JButton("RESET");
+		resetButton.setBounds(300, 160, 100, 40);
+		resetButton.setFont(new Font("Tahoma", Font.BOLD, 13));
 		
-		Button1.addActionListener(bl);
-		badd.addActionListener(bl);
-		bsub.addActionListener(bl);
-		bmul.addActionListener(bl);
-		bdiv.addActionListener(bl);
-		breset.addActionListener(bl);
+		equalButton.addActionListener(buttonListen);
+		addButton.addActionListener(buttonListen);
+		subButton.addActionListener(buttonListen);
+		multiplyButton.addActionListener(buttonListen);
+		divideButton.addActionListener(buttonListen);
+		resetButton.addActionListener(buttonListen);
 		
-		TF1=new JTextField("0",15);
-		TF1.setBounds(156, 42, 231, 28);
-		TF1.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		TF1.selectAll();
+		textField=new JTextField("0",15);
+		textField.setBounds(156, 42, 231, 28);
+		textField.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		textField.selectAll();
 		
-		bkg2lb = new JButton("Kg. --> lb.");
-		bkg2lb.setBounds(251, 295, 120, 25);
-		bkg2lb.setFont(new Font("Tahoma", Font.BOLD, 13));
-		blb2kg = new JButton("lb. --> Kg.");
-		blb2kg.setBounds(251, 333, 120, 25);
-		blb2kg.setFont(new Font("Tahoma", Font.BOLD, 13));
-		bkm2ml = new JButton("Km. --> ml.");
-		bkm2ml.setBounds(381, 333, 120, 25);
-		bkm2ml.setFont(new Font("Tahoma", Font.BOLD, 13));
-		bml2km = new JButton("ml. --> Km.");
-		bml2km.setBounds(381, 295, 120, 25);
-		bml2km.setFont(new Font("Tahoma", Font.BOLD, 13));
+		kgLbButton = new JButton("Kg. --> lb.");
+		kgLbButton.setBounds(251, 295, 120, 25);
+		kgLbButton.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lbKgButton = new JButton("lb. --> Kg.");
+		lbKgButton.setBounds(251, 333, 120, 25);
+		lbKgButton.setFont(new Font("Tahoma", Font.BOLD, 13));
+		kmMlButton = new JButton("Km. --> ml.");
+		kmMlButton.setBounds(381, 333, 120, 25);
+		kmMlButton.setFont(new Font("Tahoma", Font.BOLD, 13));
+		mlKmButton = new JButton("ml. --> Km.");
+		mlKmButton.setBounds(381, 295, 120, 25);
+		mlKmButton.setFont(new Font("Tahoma", Font.BOLD, 13));
 		
-		bkg2lb.addActionListener(bl);
-		blb2kg.addActionListener(bl);
-		bkm2ml.addActionListener(bl);
-		bml2km.addActionListener(bl);
+		kgLbButton.addActionListener(buttonListen);
+		lbKgButton.addActionListener(buttonListen);
+		kmMlButton.addActionListener(buttonListen);
+		mlKmButton.addActionListener(buttonListen);
 		
-		Pan.setLayout(null);
+		mainPanel.setLayout(null);
 	
-		Pan.add(LB1);
-		Pan.add(TF1);
-		Pan.add(badd);
-		Pan.add(bsub);
-		Pan.add(bmul);
-		Pan.add(bdiv);
-		Pan.add(Button1);
-		Pan.add(breset);
-		Pan.add(bkg2lb);
-		Pan.add(blb2kg);
-		Pan.add(bkm2ml);
+		mainPanel.add(valueLabel);
+		mainPanel.add(textField);
+		mainPanel.add(addButton);
+		mainPanel.add(subButton);
+		mainPanel.add(multiplyButton);
+		mainPanel.add(divideButton);
+		mainPanel.add(equalButton);
+		mainPanel.add(resetButton);
+		mainPanel.add(kgLbButton);
+		mainPanel.add(lbKgButton);
+		mainPanel.add(kmMlButton);
 		
-		TFCon=new JTextField("0",15);
-		TFCon.setBounds(53, 313, 171, 22);
-		TFCon.selectAll();
+	
+		mainPanel.add(mlKmButton);
 		
-		Pan.add(TFCon);
-		Pan.add(bml2km);
-		
-		getContentPane().add(Pan);
+		getContentPane().add(mainPanel);
 		
 	
 		
@@ -140,97 +136,97 @@ class ListenForButton implements ActionListener{
 	        // Check if the source of the event was the button
 	         
 	        
-	        if(e.getSource() == badd){
+	        if(e.getSource() == addButton){
 	        	
 	          
-	        	   num1=Float.parseFloat(TF1.getText());
+	        	   num1=Double.parseDouble(textField.getText());
 	               
-	               TF1.setText("");
-	               TF1.grabFocus();
+	               textField.setText("");
+	               textField.grabFocus();
 	               decide=1;
 	        	   }
 	        
 	        
-	        if(e.getSource() == bsub){
+	        if(e.getSource() == subButton){
 	        	
 	          
-	        	   num1=Float.parseFloat(TF1.getText());
+	        	   num1=Double.parseDouble(textField.getText());
 	               
-	               TF1.setText("");
-	               TF1.grabFocus();
+	               textField.setText("");
+	               textField.grabFocus();
 	               decide=2;
 	        	   }
 	        	
 	        
-	        if(e.getSource() == bmul){
+	        if(e.getSource() == multiplyButton){
 	        	
 	          
-	        	   num1=Float.parseFloat(TF1.getText());
+	        	   num1=Double.parseDouble(textField.getText());
 	               
-	               TF1.setText("");
-	               TF1.grabFocus();
+	               textField.setText("");
+	               textField.grabFocus();
 	               decide=3;
 	        	   }
 	        
 	        
-	        if(e.getSource() == bdiv){
+	        if(e.getSource() == divideButton){
 	        	
 	          
-	        	   num1=Float.parseFloat(TF1.getText());
+	        	   num1=Double.parseDouble(textField.getText());
 	               
-	               TF1.setText("");
-	               TF1.grabFocus();
+	               textField.setText("");
+	               textField.grabFocus();
 	               decide=4;
 	        	   }
 	        
 	        
-	        if(e.getSource() == Button1)
+	        if(e.getSource() == equalButton)
 	        {
-	        	num2=Float.parseFloat(TF1.getText());
+	        	num2=Double.parseDouble(textField.getText());
 	        	if(decide == 1)
 	        		{
 	        		result=num1+num2;
-	        		TF1.setText(result+"");
+	        		textField.setText(result+"");
 	        		}
 	        	if(decide == 2)
         		{
         		result=num1-num2;
-        		TF1.setText(result+"");
+        		textField.setText(result+"");
         		}
 	        	if(decide == 3)
         		{
         		result=num1*num2;
-        		TF1.setText(result+"");
+        		textField.setText(result+"");
         		}
 	        	if(decide == 4)
         		{
         		result=num1/num2;
-        		TF1.setText(result+"");
+        		textField.setText(result+"");
         		}
 	        	
 	        }
 	        
-	        if(e.getSource() == breset)
+	        if(e.getSource() == resetButton)
 	        {
 	        	num1=0;num2=0;decide=0;result=0;
-	        	TF1.setText("");TF1.grabFocus();
+	        	textField.setText("");textField.grabFocus();
 	        }
 	        
-	        if(e.getSource() == bkm2ml)
+	        if(e.getSource() == kmMlButton)
 	        {
-	        	TFCon.setText(0.621371*(Float.parseFloat(TFCon.getText()))+"");
+	        	textField.setText(0.621371*(Double.parseDouble(textField.getText()))+"");
 	        }
-	        if(e.getSource() == bml2km)
+	        if(e.getSource() == mlKmButton)
 	        {
-	        	TFCon.setText(1.609371*(Float.parseFloat(TFCon.getText()))+"");
+	        	textField.setText(1.609371*(Double.parseDouble(textField.getText()))+"");
 	        }
-	        if(e.getSource() == bkg2lb)
+	        if(e.getSource() == kgLbButton)
 	        {
-	        	TFCon.setText(2.20462*(Float.parseFloat(TFCon.getText()))+"");
+	        	textField.setText(2.20462*(Double.parseDouble(textField.getText()))+"");
 	        }
-	        if(e.getSource() == blb2kg)
+	        if(e.getSource() == lbKgButton)
 	        {
-	        	TFCon.setText(0.453592*(Float.parseFloat(TFCon.getText()))+"");
+	        	textField.setText(0.453592*(Double.parseDouble(textField.getText()))+"");
 	        }
 	    }
 
